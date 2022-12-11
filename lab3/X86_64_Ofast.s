0b09355326cd44a82b8677f6f22c4b12eff1297a
Lev <l.kurashchenko@gmail.com>
2022-12-11 22:33:51 +0700

    First commit

--- lab3/X86_64_Ofast.s
+++ lab3/X86_64_Ofast.s
@@ -7,18 +7,30 @@ main:
         xorl    %eax, %eax
         movsd   .LC6(%rip), %xmm5           //0, 10..0
         movdqa  .LC0(%rip), %xmm2           //0, 1, 2, 3
-        movdqa  .LC2(%rip), %xmm7
+        movdqa  .LC2(%rip), %xmm7           //1,1,1,1
         pxor    %xmm3, %xmm3                //Зануляем xmm3, xmm4
         pxor    %xmm4, %xmm4
-        movdqa  .LC1(%rip), %xmm8
+        movdqa  .LC1(%rip), %xmm8           //4,4,4,4
         unpcklpd        %xmm6, %xmm6        //Инструкция sse2 на перенос нижних упакованных значений double с чередованием
         unpcklpd        %xmm5, %xmm5
+
+//Этот блок повторяет то, что  выполняется в теле главного цикла в O0, но в развернутом цикле на 2 векторах по два значения и со счетчиком N/4
 .L2:
+        //Копируем значения
         movdqa  %xmm2, %xmm0
         movapd  %xmm6, %xmm10
         movdqa  %xmm4, %xmm11
+
+        //Прибавляем счетчик
         addl    $1, %eax
+
+        //Копируем значения
         movdqa  %xmm0, %xmm1
+
+        //Побитовое и, побитовое сложение,
+        //сдвиг влево, сравнение равенства,
+        //преобразование dword в double, деление,
+        //перераспределние упакованных слов и по новой
         pand    %xmm7, %xmm0
         paddd   %xmm8, %xmm2
         pslld   $1, %xmm1
@@ -46,8 +58,12 @@ main:
         orpd    %xmm9, %xmm0
         addpd   %xmm0, %xmm1
         addpd   %xmm1, %xmm3
+
+        //Перемещение по метке в цикл
         cmpl    $525000000, %eax
         jne     .L2
+
+        //Вывод числа
         movapd  %xmm3, %xmm0
         movl    $.LC7, %edi
         movl    $1, %eax
